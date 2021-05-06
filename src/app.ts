4f412c4bc77c316a90c89fc301b471221c225230
gabrielkf <gabrielkf@gmail.com>
2021-05-05 23:31:37 -0300

    Set Heroku deploy

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,15 @@
 import express from 'express';
+import cors from 'cors';
 import routes from './routes/index';
 
+const PORT = process.env.PORT || 8080;
+
 const app = express();
+
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-const PORT = 3333;
-
 app.listen(PORT, () => {
-  // eslint-disable-next-line no-console
-  console.log(`> Listening on port ${PORT}`);
+  console.log(`*** Listening on port ${PORT} ***`);
 });
