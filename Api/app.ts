8b58ce4fa853391f6bfeba29f7be92a2ad940c22
gabrielkf <gabrielkf@gmail.com>
2021-05-07 00:34:15 -0300

    Changed dir name for Heroku

--- Api/app.ts
+++ Api/app.ts
@@ -1,15 +1,21 @@
 import express from 'express';
 import cors from 'cors';
+import { join } from 'path';
 import routes from './routes';
 
 const PORT = process.env.PORT || 8080;
 
 const app = express();
 
+process.env.PWD = process.cwd();
+app.use(
+  '/img',
+  express.static(join(process.env.PWD, 'public'))
+);
+
 app.use(cors());
 app.use(express.json());
 
-app.use('/img', express.static(`${__dirname}/assets`));
 app.use(routes);
 
 app.listen(PORT);
